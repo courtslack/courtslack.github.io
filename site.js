18fd9c9ca63b94cfbe54d85069aeee34c19843a4
Courtney Slack <200154182@fvtc.edu>
2026-02-23 10:28:53 -0600

    Added carousel

--- site.js
+++ site.js
@@ -56,3 +56,43 @@ welcome.textContent = greeting; //display
 // button.addEventListener('click', () =>{
 //   button.textContent = `Secret in in the tree at the dead-end`
 // })
+
+
+//<!-- Carousel Assingment 5 -->
+const urls = [
+    'https://images.pexels.com/photos/1454360/pexels-photo-1454360.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    'https://images.pexels.com/photos/933964/pexels-photo-933964.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    'https://images.pexels.com/photos/267885/pexels-photo-267885.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    'https://images.pexels.com/photos/1251861/pexels-photo-1251861.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    'https://images.pexels.com/photos/1370296/pexels-photo-1370296.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+].map(url => { (new Image()).src = url; return url })
+
+const images = document.querySelectorAll('#carousel img')
+
+let currentImage = 0
+const showImages = () => {
+    const offset = currentImage % urls.length
+    images.forEach((image, index) => {
+        const imageIndex = (index + offset + urls.length) % urls.length
+        image.src = urls[imageIndex]
+    })
+}
+
+showImages()
+
+const prevBtn = document.querySelector('#prev')
+prevBtn.addEventListener('click', () => {
+  currentImage --
+  showImages()
+})
+
+const nextBtn = document.querySelector('#next')
+nextBtn.addEventListener('click', () => {
+  currentImage ++
+  showImages()
+})
+
+setInterval(() => {
+  currentImage++
+  showImages()
+}, 5000)
