6f51a48b9510bebbc7af31dddfc8c01c36957327
Courtney Slack <200154182@fvtc.edu>
2026-02-28 13:42:05 -0600

    Finished Assingment 6 the Tdo list

--- site.js
+++ site.js
@@ -96,3 +96,49 @@ setInterval(() => {
   currentImage++
   showImages()
 }, 5000)
+
+
+// asssingment 6 - todo list
+[
+    { "text": "Buy milk", "completed": false },
+    { "text": "Walk the dog", "completed": false },
+    { "text": "Do homework", "completed": false }
+]
+
+const todoList = document.querySelector('.todo-list') // <ul> element
+const input = document.querySelector('#new-todo') // text input box
+const addBnt = document.querySelector('#add-todo') // add button
+
+const todoParse = JSON.parse(localStorage.getItem('todo-list')) || [] 
+// convert stored string back into array
+// || [] ensures it is always an array
+
+const renderTodos = () => { // arrow function to create and display list items in the DOM
+  
+   todoList.innerHTML = '' // clear existing <li> elements before rebuilding
+
+   todoParse.forEach((todo) => { // loop through each todo object in the array
+    const li = document.createElement('li') // create new <li> element
+    li.textContent = todo.text // set text from todo object
+    li.classList.add('todo') // required class for assignment
+    todoList.append(li) // append <li> to the <ul>
+  })
+}
+
+  renderTodos()
+
+addBnt.addEventListener('click', () => {
+
+  const text = input.value.trim() //get input
+
+  if(!text) return //prevent empty
+      
+  todoParse.push({ text: text, completed: false }) //creat javascript object ({property name, value}) add to array
+  
+  localStorage.setItem('todo-list', JSON.stringify(todoParse)) //convert array to string save to localStorage
+
+  input.value = ' ' //clearn input
+
+  renderTodos() //rebuild list
+})
+
